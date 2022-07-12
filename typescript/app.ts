27e13c0fcc675df9cd3c7d635619f1155ccf36ea
eloso85 <eloso85@gmail.com>
2022-07-11 23:03:13 -0500

    added typescript

---
+++ typescript/app.ts
@@ -0,0 +1,18 @@
+//core type 
+    //number type  1, 5.3 , -10
+    // String type  "hi", 'hi', `hi`
+    //boolean true, false. 
+    //https://github.com/microsoft/TypeScript/issues/24925
+    export{}
+
+function add(n1, n2){
+    return n1 + n2;
+};
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+
+console.log(result);
+
